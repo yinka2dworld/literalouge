695c62985c80343b041b13b7deeb822d8f58db6e
Adeyinka <fawazmusty247@gmail.com>
2025-01-04 13:44:53 +0100

    Oluwa oshe

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
-
 
 export default defineConfig({
   plugins: [react()],
   base: '/', 
   server: {
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
-  }
-})
-
+    port: 3000, 
+    hot: true,  
+  },
+  build: {
+    outDir: 'dist', 
+    rollupOptions: {
+     
+    },
+  },
+});
 
